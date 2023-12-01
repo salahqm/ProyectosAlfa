8d4d51fee9fda47a1b3e1864cc7c216a28701111
Salah-20 <selqaddoury01@educarex.es>
2023-12-01 14:14:52 +0100

    modificado

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -4,6 +4,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 		System.out.println("Buenos dias.");
+		System.out.println("El numero 1 ");
 
 	}
 
